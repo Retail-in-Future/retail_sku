d73fa18780b604036fa09be358dbf1a4dfb0174a
Liang Gao <lianggao@thoughtworks.com>
2017-04-25 13:44:21 +0800

    docker tag only allow lowercase string

--- scripts/jenkins/run.sh
+++ scripts/jenkins/run.sh
@@ -4,7 +4,7 @@ cd ../../
 APP_NAME=$(basename $(pwd))
 
 echo ">>>========= build docker image with latest source code"
-DOCKER_IMAGE="local/$APP_NAME:latest"
+DOCKER_IMAGE=`echo "local/$APP_NAME:latest" | tr "[:upper:]" "[:lower:]"`
 docker build -t $DOCKER_IMAGE ./
 
 echo ">>>========= run application in docker container"
