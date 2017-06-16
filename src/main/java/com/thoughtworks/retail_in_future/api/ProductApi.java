228988807472ada75200814ed8fadfdf05ef05ad
qicaisheng <qicaisheng@qq.com>
2017-06-16 17:33:29 +0800

    Send Server Event to update product stock.

--- src/main/java/com/thoughtworks/retail_in_future/api/ProductApi.java
+++ src/main/java/com/thoughtworks/retail_in_future/api/ProductApi.java
@@ -5,11 +5,17 @@ import com.thoughtworks.retail_in_future.repository.ProductInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import javax.websocket.server.PathParam;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class ProductApi {
@@ -30,7 +36,27 @@ public class ProductApi {
     public void appendStock(@PathVariable("sku") String sku) {
         ProductInfo product = productInfoRepository.findFirstBySku(sku);
         product.setStock(product.getStock() + 1);
-
         productInfoRepository.save(product);
     }
-}
+
+    @RequestMapping(value = "/sse/products/{sku}/stock")
+    public SseEmitter getStock(@PathVariable("sku") String sku) {
+        ProductInfo product = productInfoRepository.findFirstBySku(sku);
+
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        service.execute(() -> {
+            try {
+                emitter.send(product.getStock());
+
+                Thread.sleep(200);
+            } catch (Exception e) {
+                e.printStackTrace();
+                emitter.completeWithError(e);
+                return;
+            }
+            emitter.complete();
+        });
+
+        return emitter;
+    }}
