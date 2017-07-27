3f4e295b93f5bb8dc76c300eb1a049d2f216249a
xyduan <xyduan@thoughtworks.com>
2017-07-27 22:00:32 +0800

    dockerfile and push

--- scripts/jenkins/build.sh
+++ scripts/jenkins/build.sh
@@ -9,15 +9,14 @@ docker build -t $DOCKER_IMAGE ./
 
 mkdir -p build
 docker rm retail_sku_mysql
-docker run --name retail_sku_mysql -e MYSQL_ROOT_PASSWORD=dev -e MYSQL_ROOT_HOST='%' -e MYSQL_DATABASE=retail_sku -d mysql/mysql-server:latest
+docker run --name retail_sku_mysql -e MYSQL_ROOT_PASSWORD=futureretail -e MYSQL_ROOT_HOST='%' -e MYSQL_DATABASE=retail -d mysql/mysql-server:latest
 docker run -i -e DOCKER_HOST_USERID="$(id -u):$(id -g)" -v gradle-cache:/root/.gradle -v $(pwd)/build:/root/project/build --rm --link retail_sku_mysql:mysql $DOCKER_IMAGE /bin/bash << "COMMANDS"
 set -o errexit -o nounset \
 \
 && echo ">>>========= run build in container" \
 && rm -rf build/* \
-&& sed -i -e "s/localhost:3306\/retail_sku/$MYSQL_PORT_3306_TCP_ADDR:$MYSQL_PORT_3306_TCP_PORT\/$MYSQL_ENV_MYSQL_DATABASE/g" src/main/resources/application.properties \
-&& sed -i -e "s/dev/$MYSQL_ENV_MYSQL_ROOT_PASSWORD/g" src/main/resources/application.properties \
-&& sed -i -e "s/dev/$MYSQL_ENV_MYSQL_ROOT_PASSWORD/g" src/main/resources/application-sit.properties \
+&& sed -i -e "s/localhost:3306\/retail/$MYSQL_PORT_3306_TCP_ADDR:$MYSQL_PORT_3306_TCP_PORT\/$MYSQL_ENV_MYSQL_DATABASE/g" src/main/resources/application.yml \
+&& sed -i -e "s/futureretail/$MYSQL_ENV_MYSQL_ROOT_PASSWORD/g" src/main/resources/application.yml \
 && ./gradlew build --info \
 \
 && echo ">>>========= changing artifacts owner" \
