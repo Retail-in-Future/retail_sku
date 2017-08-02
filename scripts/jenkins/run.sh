2cd1c525755e0dd238e3b0f98cca32b5436ad51e
dgchen <dgchen@thoughtworks.com>
2017-08-02 11:48:35 +0800

    [diangui] remove docker runw

--- scripts/jenkins/run.sh
+++ scripts/jenkins/run.sh
@@ -7,5 +7,5 @@ echo ">>>========= build docker image with latest source code"
 DOCKER_IMAGE=`echo "local/$APP_NAME:latest" | tr "[:upper:]" "[:lower:]"`
 docker build -t $DOCKER_IMAGE ./
 
-echo ">>>========= run application in docker container"
-docker run -v gradle-cache:/root/.gradle --rm $DOCKER_IMAGE ./gradlew clean assemble
+#echo ">>>========= run application in docker container"
+#docker run -v gradle-cache:/root/.gradle --rm $DOCKER_IMAGE ./gradlew clean assemble
